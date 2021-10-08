5a19dfb152f1fce14262c6153d2e86cce750a621
Xxx-Vlados-Nagibator228-xxX <91702474+Xxx-Vlados-Nagibator228-xxX@users.noreply.github.com>
2021-10-08 20:13:46 +0300

    Add files via upload

---
+++ gfsg.java
@@ -0,0 +1,18 @@
+package mycompany.my;
+
+import java.util.Scanner;
+
+public class gfsg {
+    public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Введите трехзначное число ");
+        int g = scanner.nextInt();
+        int[] massiv = new int[123];
+        int a = massiv[0];
+        int b = massiv[1];
+        int c = massiv[2];
+        System.out.print(a);
+        System.out.print(b);
+        System.out.print(c);
+    }
+}
